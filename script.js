b57f3b9eed8508aae00daf32581c0308bdb24788
kawekaweau <cryptoblockchainjunkie@gmail.com>
2023-01-25 03:18:43 -0500

    added about me section lost script at the end

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 
 // Mouse-circle
-const mouseCircle = document.querySelector('.mouse-circle')
-const mouseDot = document.querySelector('.mouse-dot')
+const mouseCircle = document.querySelector('.mouse-circle');
+const mouseDot = document.querySelector('.mouse-dot');
 const mouseCircleFn = (x, y) => {
     mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
     mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
-
 // End of Circle
 
-
 // Animated Circles
-
 const circles = document.querySelectorAll('.circle');
 const mainImg = document.querySelector('.main-circle img');
 
+let mX = 0;
+let mY = 0;
+const z = 100;
 
 const animateCircles = (e, x, y) => {
-
-    let mX = 0;
-    let mY = 0;
-    const z = 100;
-
-
-    if (x < mX) {
+    if(x < mX) {
         circles.forEach((circle) => {
             circle.style.left = `${z}px`;
         });
@@ -35,7 +29,7 @@ const animateCircles = (e, x, y) => {
         mainImg.style.left = `-${z}px`;
     }
 
-    if (y < mY) {
+    if(y < mY) {
         circles.forEach((circle) => {
             circle.style.top = `${z}px`;
         });
@@ -70,19 +64,37 @@ document.body.addEventListener('mouseleave', () => {
 // Main Button
 const mainBtn = document.querySelector('.main-btn');
 
-let ripple;
+mainBtns.forEach(btn => {
+    let ripple;
 
-mainBtn.addEventListener('mouseenter', e => {
-    const left = e.clientX - e.target.getBoundingClientRect().left;
-    const top = e.clientY - e.target.getBoundingClientRect().top;
+    btn.addEventListener('mouseenter', e => {
+        const left = e.clientX - e.target.getBoundingClientRect().left;
+        const top = e.clientY - e.target.getBoundingClientRect().top;
 
-    ripple = document.createElement('div');
-    ripple.classList.add('ripple');
-    ripple.style.left = `${left}px`;
-    ripple.style.top = `${top}px`;
-    mainBtn.prepend(ripple);
+        ripple = document.createElement('div');
+        ripple.classList.add('ripple');
+        ripple.style.left = `${left}px`;
+        ripple.style.top = `${top}px`;
+        btn.prepend(ripple);
+    });
+
+    btn.addEventListener('mouseleave', () => {
+        btn.removeChild(ripple);
+    });
 });
+// End of Main Button
 
-mainBtn.addEventListener('mouseleave', () => {
-    mainBtn.removeChild(ripple);
+// Section 2
+const aboutMeText = document.querySelector('.about-me-text');
+const aboutMeTextContent = 'I am a programmer, Focusing on Web Design, Blockchain, Deep Learning and CyberSecurity.' +
+    ' Want to know more? Contact me!';
+
+Array.from(aboutMeTextContent).forEach(char => {
+    const span = document.createElement('span')
+    span.textContent = char;
+    aboutMeText.appendChild(span);
+
+//    span.addEventListener('mouseenter', (e) => {
+//        e.target.style.animation = "aboutMeTextAnim 10s infinite"
+//    });
 });
